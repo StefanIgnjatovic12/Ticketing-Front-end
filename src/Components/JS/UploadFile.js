98cf89ca578ec032eca7f93ccae5dc6d5652af58
Stefan <sper1234@gmail.com>
2022-03-09 16:57:37 +0100

    Add icon to open upload dialog to the table toolbar

--- src/Components/JS/UploadFile.js
+++ src/Components/JS/UploadFile.js
@@ -1,10 +1,18 @@
+import {withStyles} from "@material-ui/core/styles";
 import FileUpload from 'react-material-file-upload';
 import Box from "@mui/material/Box";
 import Dialog from '@mui/material/Dialog';
 import {useState} from "react";
-import Button from "@mui/material/Button";
+import CloudUploadIcon from '@mui/icons-material/CloudUpload';
+import IconButton from '@mui/material/IconButton';
+import Tooltip from '@mui/material/Tooltip';
 
-export default function UploadFile(props) {
+
+const defaultToolbarStyles = {
+    iconButton: {},
+};
+
+function UploadFile(props) {
     const [open, setOpen] = useState(false)
     const handleClickOpen = () => {
         setOpen(true);
@@ -13,20 +21,20 @@ export default function UploadFile(props) {
     const handleClose = () => {
         setOpen(false);
     };
-
     return (
         <>
-            <Button variant="outlined" onClick={handleClickOpen}>
-                Upload attachment
-            </Button>
+            <Tooltip title="Upload attachment">
+                <IconButton onClick={handleClickOpen}>
+                    <CloudUploadIcon/>
+                </IconButton>
+            </Tooltip>
             <Dialog open={open} onClose={handleClose} fullWidth={true} maxWidth={"xs"}>
-
                 <Box>
                     <FileUpload value={props.files} onChange={props.setFiles}/>
                 </Box>
-
             </Dialog>
         </>
-
     )
 }
+
+export default withStyles(defaultToolbarStyles, {name: "UploadFile"})(UploadFile);
