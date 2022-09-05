a0bba2bacce36121b5d5e86fc3e7b8bd72b5b0e7
Stefan <sper1234@gmail.com>
2022-09-05 18:39:15 +0200

    Refactor

--- src/Components/JS/Authentication/DemoSignIn.js
+++ src/Components/JS/Authentication/DemoSignIn.js
@@ -6,7 +6,9 @@ import DialogTitle from '@mui/material/DialogTitle';
 import Link from "@mui/material/Link";
 import {useAuth} from "../UserManagement/CurrentUserContext";
 import {useNavigate} from "react-router-dom";
-
+import admin_pic from 'public/admin.png';
+import developer_pic from 'public/developer.png';
+import user_pic from 'public/user.png';
 
 export default function DemoSignIn() {
     const [open, setOpen] = useState(false)
@@ -82,7 +84,8 @@ export default function DemoSignIn() {
                                 width: "30%",
                                 cursor: "pointer"
                             }}
-                            src={process.env.PUBLIC_URL + "admin.png"}
+                            // src={process.env.PUBLIC_URL + "admin.png"}
+                            src={admin_pic}
                         >
                         </Box>
 
@@ -97,7 +100,7 @@ export default function DemoSignIn() {
                                 width: "30%",
                                 cursor: "pointer"
                             }}
-                            src={process.env.PUBLIC_URL + "developer.png"}
+                            src={developer_pic}
                         >
                         </Box>
 
@@ -112,7 +115,7 @@ export default function DemoSignIn() {
                                 cursor: "pointer"
                             }}
 
-                            src={process.env.PUBLIC_URL + "user.png"}
+                            src={user_pic}
                         >
                         </Box>
 
